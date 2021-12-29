52664599b53d9971eb8732f099493c5c866e4b8a
edilsonvilarinho <edilson.vilarinho.messias@gmail.com>
2021-12-29 00:03:22 -0300

    Ajuste para apresentar o userListProgressBar

--- app/src/main/java/br/com/edilsonvilarinho/androiddesafio/MainActivity.kt
+++ app/src/main/java/br/com/edilsonvilarinho/androiddesafio/MainActivity.kt
@@ -1,8 +1,8 @@
 package br.com.edilsonvilarinho.androiddesafio
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.edilsonvilarinho.androiddesafio.databinding.ActivityMainBinding
@@ -18,10 +18,12 @@ class MainActivity : AppCompatActivity() {
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         mainViewModel.obterContatos()
         mainViewModel.contatos.observe(this, {
+            binding.userListProgressBar.visibility = View.GONE
             var adapter = RecyclerViewAdapter(it)
             binding.recyclerView.adapter = adapter
             binding.recyclerView.layoutManager = LinearLayoutManager(this)
         })
+        binding.userListProgressBar.visibility = View.VISIBLE
         setContentView(binding.root)
     }
 }
