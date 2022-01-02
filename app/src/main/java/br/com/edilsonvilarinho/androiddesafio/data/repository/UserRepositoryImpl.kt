078647748ae3ca311ad71a39c02205e3c64c4cbb
edilsonvilarinho <edilson.vilarinho.messias@gmail.com>
2022-01-01 23:14:27 -0300

    refatorando estrutura do UserRepository

--- app/src/main/java/br/com/edilsonvilarinho/androiddesafio/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/br/com/edilsonvilarinho/androiddesafio/data/repository/UserRepositoryImpl.kt
@@ -6,15 +6,16 @@ import br.com.edilsonvilarinho.androiddesafio.data.local.datasource.user.UserLoc
 import br.com.edilsonvilarinho.androiddesafio.data.local.db.entity.toUser
 import br.com.edilsonvilarinho.androiddesafio.data.model.User
 import br.com.edilsonvilarinho.androiddesafio.data.model.toUserEntity
+import br.com.edilsonvilarinho.androiddesafio.data.remote.datasource.user.UserRemoteDataSource
 import br.com.edilsonvilarinho.androiddesafio.data.remote.datasource.user.UserRemoteDataSourceImpl
 
-class UserRepositoryImpl(val context: Context) : UserRepository {
-
-    private val userLocalDataSource: UserLocalDataSource = UserLocalDataSourceImpl(context)
-
-
+class UserRepositoryImpl(
+    context: Context,
+    val userRemoteDataSource: UserRemoteDataSource = UserRemoteDataSourceImpl(),
+    val userLocalDataSource: UserLocalDataSource = UserLocalDataSourceImpl(context)
+) : UserRepository {
     override suspend fun getUsers(): List<User>? {
-        val users: List<User> = UserRemoteDataSourceImpl().getUsers()
+        val users: List<User> = userRemoteDataSource.getUsers()
         userLocalDataSource.insertUsers(users.map { it.toUserEntity() })
         return userLocalDataSource.getUsers().map { it.toUser() }
     }
